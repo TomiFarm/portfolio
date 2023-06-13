6fb9d6aaf05fd4579cf1652ff4f7477b4fcf59f8
TomiFarm <117461271+TomiFarm@users.noreply.github.com>
2023-06-13 13:56:27 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import { PageHeader } from './components/page-header/page-header';
+import { MainView } from './components/main-view/main-view';
+import { AboutView } from './components/about-view/about-view';
+import { ProjectView } from './components/project-view/project-view';
+import { ContactView } from './components/contact-view/contact-view';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+    <BrowserRouter>
+      <PageHeader />
+      <div className='main-container'>
+        <Routes>
+          <Route
+            path="/"
+            element={<MainView />}
+          />
+          <Route
+            path="/about"
+            element={<AboutView />}
+          />
+          <Route
+            path="/projects"
+            element={<ProjectView />}
+          />
+          <Route
+            path="/contact"
+            element={<ContactView />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
